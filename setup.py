0f700f79b98f0447b61114a079f3af05f49b21be
Victor E <victorevector@gmail.com>
2017-01-31 11:08:56 -0800

    dafsd

--- setup.py
+++ setup.py
@@ -30,14 +30,14 @@ class Install_Deps_Dev(develop, Install_Deps):
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "testing_dependency_update"
+plugin_identifier = "octoprint_testing_dependency_update"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_testing_dependency_update"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "testing_dependency_update"
+plugin_name = "octoprint_testing_dependency_update"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.0"
